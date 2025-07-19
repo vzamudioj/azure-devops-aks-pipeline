c4fe25c728f335447e99d22a617b844a041cee22
vzamudio-rabbitmx <89927588+vzamudio-rabbitmx@users.noreply.github.com>
2025-07-19 16:00:03 -0600

    add cert and nginx controller

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -8,6 +8,21 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.75.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.1.0"
+    }
+  }
+}
+
+
+
 resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   name                = "${var.cluster_name}_${var.environment}"
   location            = azurerm_resource_group.resource_group.location
@@ -45,5 +60,49 @@ terraform {
     # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
     # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
     # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
+  }
+}
+
+data "azurerm_kubernetes_cluster" "this" {
+  name                = "${var.cluster_name}_${var.environment}"
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  # Comment this out if you get: Error: Kubernetes cluster unreachable 
+  depends_on = [azurerm_kubernetes_cluster.this]
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.azurerm_kubernetes_cluster.this.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "external_nginx" {
+  name = "external"
+
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress"
+  create_namespace = true
+  version          = "4.8.0"
+
+  values = [file("${path.module}/values/ingress.yaml")]
+}
+
+resource "helm_release" "cert_manager" {
+  name = "cert-manager"
+
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+  version          = "v1.13.1"
+
+  set {
+    name  = "installCRDs"
+    value = "true"
   }
 }
