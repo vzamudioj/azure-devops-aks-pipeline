27747d03a55b6e71efbf9dac535fde7dfea6f54e
vzamudio-rabbitmx <89927588+vzamudio-rabbitmx@users.noreply.github.com>
2025-07-19 16:43:46 -0600

    add cert and nginx controller

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -6,20 +6,21 @@ resource "azurerm_resource_group" "resource_group" {
 provider "azurerm" {
   //version = "~>2.0.0"
   features {}
+   skip_provider_registration = true
 }
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.75.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.1.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "3.75.0"
+#     }
+#     helm = {
+#       source  = "hashicorp/helm"
+#       version = ">= 2.1.0"
+#     }
+#   }
+# }
 
 
 
